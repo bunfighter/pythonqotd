44c390620a673af5356a6ff673b4e1fab1321da3
Don Reid <86226009+ReidDon@users.noreply.github.com>
2022-03-14 21:08:17 +0200

    Added the python script

---
+++ pythonquote.py
@@ -0,0 +1,58 @@
+import random
+
+quotelist = ["Are You Suggesting That Coconuts Migrate?",
+"Every Time I Try To Talk To Someone It’s ‘Sorry This’ And ‘Forgive Me That’ And ‘I’m Not Worthy.’",
+"I Didn't Know You Were Called Dennis!",
+"Strange Women Lyin' In Ponds Distributin' Swords Is No System For A Basis Of Government.",
+"I am an enchanter. There Are Those Who Call Me...Tim.",
+"Your Mother Was A Hamster And Your Father Smelt Of Elderberries!",
+"Run Away! Run Away!",
+"He's not the Messiah—he's a very naughty boy!",
+"My brain hurts!",
+"There's nothing wrong with you that an expensive operation can't prolong.",
+"Oh! Now we see the violence inherent in the system! Help, help, I'm being repressed!",
+"There are a great many people in the country today, who through no fault of their own, are sane.",
+"It's just a flesh wound.",
+"Nobody expects the Spanish inquisition!",
+"One day, lad, all this will be yours.",
+"On second thoughts, let us not go to Camelot. It is a silly place.",
+"Blessed are the cheesemakers'.",
+"It's only a wafer-thin mint, sir...",
+"Have you got anything without spam?"]
+
+print("""
+
+               ________________________________________
+  ==================================================================
+
+      ::::    ::::   ::::::::  ::::    ::: ::::::::::: :::   :::
+      +:+:+: :+:+:+ :+:    :+: :+:+:   :+:     :+:     :+:   :+:
+      +:+ +:+:+ +:+ +:+    +:+ :+:+:+  +:+     +:+      +:+ +:+
+      +#+  +:+  +#+ +#+    +:+ +#+ +:+ +#+     +#+       +#++:
+      +#+       +#+ +#+    +#+ +#+  +#+#+#     +#+        +#+
+      #+#       #+# #+#    #+# #+#   #+#+#     #+#        #+#
+      ###       ###  ########  ###    ####     ###        ###
+
+  :::::::::  :::   ::: ::::::::::: :::    :::  ::::::::  ::::    :::
+  :+:    :+: :+:   :+:     :+:     :+:    :+: :+:    :+: :+:+:   :+:
+  +:+    +:+  +:+ +:+      +:+     +:+    +:+ +:+    +:+ :+:+:+  +:+
+  +#++:++#+    +#++:       +#+     +#++:++#++ +#+    +:+ +#+ +:+ +#+
+  +#+           +#+        +#+     +#+    +#+ +#+    +#+ +#+  +#+#+#
+  #+#           #+#        #+#     #+#    #+# #+#    #+# #+#   #+#+#
+  ###           ###        ###     ###    ###  ########  ###    ####
+
+               ::::::::   :::::::: ::::::::::: :::::::::
+              :+:    :+: :+:    :+:    :+:     :+:    :+:
+              +:+    +:+ +:+    +:+    +:+     +:+    +:+
+              +#+    +:+ +#+    +:+    +#+     +#+    +:+
+              +#+  # +#+ +#+    +#+    +#+     +#+    +#+
+              #+#   +#+  #+#    #+#    #+#     #+#    #+#
+               ###### ### ########     ###     #########
+
+  ==================================================================
+               ----------------------------------------
+
+
+  """ + random.choice(quotelist))
+print("")
+print("")
